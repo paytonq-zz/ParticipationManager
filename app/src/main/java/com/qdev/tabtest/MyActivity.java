1aa17b511021847b35f5eeeb316d743507ce6401
Payton Quinn <paytonq@uw.edu>
2015-03-16 23:51:36 -0700

    relicense and minor changes

--- app/src/main/java/com/qdev/tabtest/MyActivity.java
+++ app/src/main/java/com/qdev/tabtest/MyActivity.java
@@ -1,6 +1,17 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* Copyright (C) 2015 Payton Quinn
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 package com.qdev.tabtest;
 
@@ -96,14 +107,14 @@ public class MyActivity extends Activity  {
                 builder.setMessage("For instructions on using this application, please tap " +
                         "'Visit Homepage' below.\n\n" +
                         "Participation Manager is licensed under " +
-                        "the terms of the Mozilla Public License v2.0.  " +
-                        "To view the terms of the Mozilla Public License v2.0 " +
+                        "the terms of the GNU General Public License v3.0.  " +
+                        "To view the terms of the GNU General Public License v3.0 " +
                         "tap 'License' below.").setCancelable(true);
                 builder.setPositiveButton("License", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
 
                         Intent browserIntent = new Intent(Intent.ACTION_VIEW,
-                                Uri.parse("http://opensource.org/licenses/MPL-2.0"));
+                                Uri.parse("https://www.gnu.org/licenses/gpl-3.0.txt"));
                         startActivity(browserIntent);
                     }
                 });
--- app/src/main/java/com/qdev/tabtest/newClasses.java
+++ app/src/main/java/com/qdev/tabtest/newClasses.java
@@ -1,6 +1,17 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* Copyright (C) 2015 Payton Quinn
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 package com.qdev.tabtest;
 
@@ -19,7 +30,7 @@ public class newClasses extends Fragment {
                              Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.tab2, container, false);
         TextView textview = (TextView) view.findViewById(R.id.tabtextview);
-        textview.setText("You haven't added any classes yet.  Press the plus sign to get started!");
+        textview.setText("You haven't added any classes yet.\nPress the plus sign to get started!");
         return view;
     }
 }
